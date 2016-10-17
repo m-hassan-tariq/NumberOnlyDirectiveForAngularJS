1f6a3a31c3490cd6688c6638dfa92bc18fd6707b
Muhammad Hassan <hassantariq.8631@gmail.com>
2016-10-17 12:15:40 -0500

    Number Only Directive For AngularJS
    Custom AngularJS directive to allow number only as per user key input or
    through paste/cut

---
+++ script.js
@@ -0,0 +1,36 @@
+﻿/**
+* Custom AngularJS directive to allow number only
+* as per user key input
+*/
+(function () {
+    'use strict';
+ 
+    angular
+        .module('sampleApp' , [])
+        .directive('numbersOnly', numbersOnly);
+ 
+    numbersOnly.$inject = [];
+ 
+    function numbersOnly() {
+ 
+        var directive = {
+            restrict: 'A',
+			require: 'ngModel',
+            link: function (scope, element, attr, ngModelCtrl) {
+				function fromUser(text) {
+					if (text) {
+						var transformedInput = text.replace(/[^0-9-]/g, '');
+						if (transformedInput !== text) {
+							ngModelCtrl.$setViewValue(transformedInput);
+							ngModelCtrl.$render();
+						}
+						return transformedInput;
+					}
+					return undefined;
+				}
+				ngModelCtrl.$parsers.push(fromUser);
+			}
+		};
+        return directive;
+    };
+})();
